67eb872db4e2b0302f7b65b61a61599522ce43c3
adarshnarayan04 <adarshnarayangumla@gmail.com>
2024-10-07 01:25:51 +0530

    check

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
-import TextChange from "../Textchange.jsx";
+// import TextChange from "../Textchange";
 
 export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>
-          {/* Hi, I'm Adarsh */}
-          <TextChange />
+          Hi, I'm Adarsh
+          {/* <TextChange /> */}
           </h1>
         <p className={styles.description}>
           I'm a full-stack developer with experience using React and NodeJS.
