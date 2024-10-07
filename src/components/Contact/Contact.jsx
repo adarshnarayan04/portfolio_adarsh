f62dc3e453b76a514e20e9a6866b74a3ca4f1703
adarshnarayan04 <adarshnarayangumla@gmail.com>
2024-10-07 13:50:09 +0530

    improved the design

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,18 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:adarshnarayangumla@gmail.com">adarshnarayangumla@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="linkedin.com/in/adarsh-narayan-711a24263">linkedin.com/in/adarsh-narayan-711a24263</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://github.com/adarshnarayan04">https://github.com/adarshnarayan04</a>
         </li>
       </ul>
     </footer>
